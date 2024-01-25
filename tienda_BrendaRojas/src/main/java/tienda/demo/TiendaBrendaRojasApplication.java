bec63b89232b88d2b1b47c5852b3108a3235b8cc
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-01-24 18:34:00 -0600

    creacion de proyecto

---
+++ tienda_BrendaRojas/src/main/java/tienda/demo/TiendaBrendaRojasApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBrendaRojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBrendaRojasApplication.class, args);
+	}
+
+}
