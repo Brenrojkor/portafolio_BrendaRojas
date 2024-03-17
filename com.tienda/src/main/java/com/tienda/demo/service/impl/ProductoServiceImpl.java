c47482ef9c91ad824b7cbd874795c0a0e7000edc
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-03-17 12:10:24 -0600

    intentoCorreción clase sábado

--- com.tienda/src/main/java/com/tienda/demo/service/impl/ProductoServiceImpl.java
+++ com.tienda/src/main/java/com/tienda/demo/service/impl/ProductoServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-class ProductoServiceImpl implements ProductoService {
+public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
     private ProductoDao productoDao;
@@ -44,8 +44,9 @@ class ProductoServiceImpl implements ProductoService {
     @Transactional
     public void delete(Producto producto) {
         productoDao.delete(producto);
-    }    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada 
+    }
 
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
     @Override
     @Transactional(readOnly = true)
     public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
@@ -63,4 +64,5 @@ class ProductoServiceImpl implements ProductoService {
     public List<Producto> metodoNativo(double precioInf, double precioSup) {
         return productoDao.metodoNativo(precioInf, precioSup);
     }
+
 }
